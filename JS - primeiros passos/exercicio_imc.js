8e186061ac901e71d2ffd9cbc42d1143354a545f
Pedro <phps.92@gmail.com>
2022-11-12 16:39:58 -0300

    Exercício sobre IMC

---
+++ JS - primeiros passos/exercicio_imc.js
@@ -0,0 +1,26 @@
+/* IMC = peso / (altura * altura)
+
+Elabore um algoritmo que dado o peso e a altura de um adulto, mostre sua condição de acordo com a tabela abaixo:
+
+IMC em adultos:
+- Abaixo de 18.5 = abaixo do peso;
+- Entre 18 e 25  = peso normal;
+- Entre 25 e 30 = acima do peso;
+- Entre 30 e 40 = obeso;
+*/
+
+
+const pesoEmKg = 80;
+const altura = 1.80;
+const imc = pesoEmKg / (altura * altura);
+
+if (imc < 18.5) {
+    console.log(imc);
+
+} else if (imc >= 18 && imc <= 25) {
+    console.log(imc);
+
+} else {
+    console.log(imc);
+
+}
