85bd8730afb5fff19cdc549dc25caf0bf28607eb
Pedro <phps.92@gmail.com>
2022-11-13 14:26:45 -0300

    Finalizando a Classe de pessoas e exemplo final.

--- JS - primeiros passos/pessoa.js
+++ JS - primeiros passos/pessoa.js
@@ -7,21 +7,27 @@ class Pessoa{
         this.nome = nome;
         this.idade = idade;
         this.anoDeNascimento = 2022 - idade;
-
     }
-    
+
     descrever(){
         console.log(`Meu nome é: ${this.nome}, e minha idade é: ${this.idade} e eu nasci em:
         ${this.anoDeNascimento}`);
 
     }
-
 }
-    const pedro = new Pessoa('Pedro', 31, 1992);
-    const carlos = new Pessoa('Carlos', 23, 1987);
 
-pedro.descrever();      //imprimindo apenas o resultado do método
-carlos.descrever();
 
-console.log(pedro);    
-console.log(carlos);
+function comparaPessoas(p1, p2){
+    if(p1.idade > p2.idade){
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome} !`);
+    } else if(p2.idade > p1.idade){
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome} !`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} têm o mesmo nome!`);
+    }
+}
+
+const pedro = new Pessoa('Pedro', 30);
+const ana = new Pessoa('Ana', 45);
+
+(comparaPessoas(pedro, ana));
