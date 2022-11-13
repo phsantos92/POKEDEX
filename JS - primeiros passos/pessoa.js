59e2ea6e43071f0e1ed5b868d2026ddd2abe7004
Pedro <phps.92@gmail.com>
2022-11-13 13:55:29 -0300

    Um pouco sobre classes

--- JS - primeiros passos/pessoa.js
+++ JS - primeiros passos/pessoa.js
@@ -1,10 +1,22 @@
+class Pessoa{
+    nome;
+    idade;
+
+    descrever(){
+        console.log(`Meu nome é: ${this.nome}, e minha idade é: ${this.idade}`);
+
+    }
+
+}
+
 const pessoa = {
-    nome: pedro,
+    nome: "pedro",
     idade: 30,
 
     descrever: function(){
 
-        console.log('Meu nome é: ${this.nome}');
+        // demorei bastante para notar que nesse caso usamos ASPAS INVERTIDAS
+        console.log(`Meu nome é: ${this.nome}, e minha idade é: ${this.idade}`);
 
     }
 };
