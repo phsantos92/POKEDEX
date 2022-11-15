ca31d677290109d6ab013a75f3d6719a6fa7c65c
Pedro <phps.92@gmail.com>
2022-11-15 13:49:04 -0300

    outra forma de fazer o for.

--- JS - primeiros passos/imprimindonomefor.js
+++ JS - primeiros passos/imprimindonomefor.js
@@ -5,6 +5,7 @@ let nome = 'Pedro Henrique Pereira dos Santos';
 
 // índice; condição a ser respeitada; iteração.
 for (let i = 0; i < nome.length; i++) {
-    console.log(nome[i]);
+    const letra = nome[i];   // variável letra vai receber cada letra da cadeia de string NOME.
+    console.log(letra);
     
 }
