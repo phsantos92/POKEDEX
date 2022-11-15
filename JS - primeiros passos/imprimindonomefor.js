4f141f3d95cb5fb7b5da35b3cd23bb1ee3bd0293
Pedro <phps.92@gmail.com>
2022-11-15 13:40:56 -0300

    Começando a usar for.

---
+++ JS - primeiros passos/imprimindonomefor.js
@@ -0,0 +1,10 @@
+
+let nome = 'Pedro Henrique Pereira dos Santos';
+
+
+
+// índice; condição a ser respeitada; iteração.
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i]);
+    
+}
--- JS - primeiros passos/pessoasIMCobj.js
+++ JS - primeiros passos/pessoasIMCobj.js
@@ -5,24 +5,45 @@ Instancie uma pessoa chamada José que tenha 70 kg e 1,75 de altura. E peça a J
 
 */
 
-class Pessoa1{
+class Pessoa1 {
     nome;
     peso;
     altura;
 
-    constructor(nome, peso, altura){
+    constructor(nome, peso, altura) {
         this.nome = nome;
         this.peso = peso;
         this.altura = altura;
     }
-    calcularIMC(peso, altura){          // ERRO --> NÃO PRECISA RECEBER NADA EXTERNO, tudo já vai ser informado através do this.
+
+    classificarIMC() {
+        const imc = this.calcularIMC();
+
+        if (imc < 18.5){
+            return ('Abaixo do peso');
+        } else if(imc >= 18.5 && imc <= 25){
+            return ('No peso');
+        } else if(imc >= 25 && imc <= 30){
+            return ('Obeso');
+        } else {
+            return ('Obesidade mórbida');
+        }
+        
+}
+    calcularIMC() {
         return this.peso / (this.altura * this.altura);
 
     }
 }
 
-    const jose = new Pessoa1('José', 78, 100);
+const jose = new Pessoa1('José', 70, 1.75);
+console.log(jose.calcularIMC());
+console.log(jose.classificarIMC());
+
+const pedro = new Pessoa1('Pedro', 88, 1.84);
+console.log(pedro.calcularIMC());
+console.log(pedro.classificarIMC());
 
 
 
-}
+
