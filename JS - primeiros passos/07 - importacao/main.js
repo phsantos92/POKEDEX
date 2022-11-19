4fbb1005a218c9f61487804ac201beb75f433630
Pedro <phps.92@gmail.com>
2022-11-18 23:11:12 -0300

    criando e destruindo um objeto

--- JS - primeiros passos/07 - importacao/main.js
+++ JS - primeiros passos/07 - importacao/main.js
@@ -1,3 +1,13 @@
-const funcoes = require('./funcoes-auxiliares');
+const funcoes = require('./funcoes-auxiliares');    // objeto funcoes recebe o arquivo a ser importado.
 
 console.log(funcoes);
+
+const pessoa = {
+
+    nome: "vitor"
+}
+
+//códigos equivalentes !!
+const {nome} = pessoa;
+
+const nome = pessoa.nome;
