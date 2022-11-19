89e9339252f2ff6582d69cd53fbfcd244060eb23
Pedro <phps.92@gmail.com>
2022-11-19 00:32:27 -0300

    Exercício - Informar maior valor entre 5 numeros (gets, sets)

--- JS - primeiros passos/07 - importacao/main.js
+++ JS - primeiros passos/07 - importacao/main.js
@@ -1,13 +1,25 @@
-const funcoes = require('./funcoes-auxiliares');    // objeto funcoes recebe o arquivo a ser importado.
+/* Dado um conjunto de números, imprima-os na tela e determine qual é o maior valor
+    números: 5, 18, 78, 65, 90. Qual é o maior?
+*/
 
-console.log(funcoes);
+const { gets, print } = require('./funcoes-auxiliares');    // objeto funcoes recebe o arquivo a ser importado.
 
-const pessoa = {
+const numerosSorteados = [];      //Inicializando uma lista vazia
 
-    nome: "vitor"
+let maiorValor = 0      //Variável iniciando com 0 para ser atribuída ao maior valor no resultado da comparação
+
+for (let i = 0; i < 5; i++) {       //enquanto i for menor que os 5 números
+    const numeroSorteado = gets();     //variável número sorteado recebe o objeto gets, que tem tem como retorno o número
+    numerosSorteados.push(numeroSorteado);
+
+    if(numeroSorteado > maiorValor){
+        maiorValor = numeroSorteado;
+    }
 }
 
-//códigos equivalentes !!
-const {nome} = pessoa;
 
-const nome = pessoa.nome;
+print(maiorValor);
+
+//preciso receber 5 números...
+
+print(gets());
