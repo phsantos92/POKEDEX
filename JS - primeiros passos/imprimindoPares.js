d3b108b36119d396db7405c4f38164ec51843180
Pedro <phps.92@gmail.com>
2022-11-18 23:01:46 -0300

    Imprimindo pares em uma lista

--- JS - primeiros passos/imprimindoPares.js
+++ JS - primeiros passos/imprimindoPares.js
@@ -1,13 +1,12 @@
 // 1 - Crie um programa que percorre uma lista e imprima os números pares.
 
-const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numerosPares = [];
 
-for (let i = 0; i <= lista.length; i++) {
-    const numeroLista = lista[i];
-
-    if(numeroLista % 2 == 0){
-        const pares = numeroLista;
-        console.log(pares);
+    for (let i = 0; i < 10; i++) {
+        const numeroPar = i % 2 === 0;
+        if (numeroPar) {
+            numerosPares.push(i);
+        }
     }
-    
-}
+
+    console.log(numerosPares);
